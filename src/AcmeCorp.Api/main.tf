7e506bcd05171c20d2ac82041f2085dcad737578
milavernazza <milavernazza@gmail.com>
2024-05-21 21:43:44 -0300

    Add IaC for AWS deployment using Terraform

--- src/AcmeCorp.Api/main.tf
+++ src/AcmeCorp.Api/main.tf
@@ -26,3 +26,42 @@ resource "aws_ecs_task_definition" "acmecorp" {
         }
     ])
 }
+
+resource "aws_ecs_service" "acmecorp" {
+    name                = "acmecorp-service"
+    cluster             = aws_ecs_cluster.acmecorp.id
+    task_definition     = aws_ecs_task_definition.acmecorp.arn
+    desired_count       = 1
+    launch_type         = "FARGATE"
+
+    network_configuration {
+        subnets = [11000000.10101000]
+        security_groups = [123456789012]
+    } 
+}
+
+resource "aws_lb" "acmecorp" {
+    name                    = "acmecorp-lb"
+    internal                = false
+    load_balancer_type      = "application"
+    security_groups         = [123456789012] 
+    subnets                 = [11000000.10101000]
+}
+
+resource "aws_lb_target_group" "acmecorp" {
+    name          = "acmecorp-tg"
+    port          = 80
+    protocol      = "HTTP"
+    vpc_id        = [123456789012] 
+}
+
+resource "aws_lb_listener" "acmecorp" {
+    load_balancer_arn = acme_lb.acmecorp.arn
+    port          = "80"
+    protocol      = "HTTP"
+    
+    default_action {
+      type = "forward"
+      target_group_arn = aws_lb_target_group.acmecorp.arn
+    }
+}
